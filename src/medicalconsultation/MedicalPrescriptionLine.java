9e845b40245b0c09935f2ca748a484d2fd093060
Xavier <xaavier@protonmail.com>
2021-01-12 12:20:43 +0100

    Done MedPrescLine and ProdSpecif classes from medicalconsultation package

--- src/medicalconsultation/MedicalPrescriptionLine.java
+++ src/medicalconsultation/MedicalPrescriptionLine.java
@@ -1,4 +1,37 @@
 package medicalconsultation;
 
+import data.ProductID;
+
 public class MedicalPrescriptionLine {
+    private ProductID prodID;
+    private TakingGuideline tg;
+
+    public MedicalPrescriptionLine(ProductID prodID, TakingGuideline tg) {
+        this.prodID = prodID;
+        this.tg = tg;
+    }
+
+    public ProductID getProductID() {
+        return prodID;
+    }
+
+    public TakingGuideline getTakingGuideline() {
+        return tg;
+    }
+
+    public void setProductID(ProductID prodID) {
+        this.prodID = prodID;
+    }
+
+    public void setTakingGuideline(TakingGuideline tg) {
+        this.tg = tg;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        MedicalPrescriptionLine mpl = (MedicalPrescriptionLine) o;
+        return this.prodID.equals(mpl.prodID) && this.tg.equals(mpl.tg);
+    }
 }
--- src/medicalconsultation/ProductSpecification.java
+++ src/medicalconsultation/ProductSpecification.java
@@ -1,4 +1,49 @@
 package medicalconsultation;
 
+import data.ProductID;
+
+import java.math.BigDecimal;
+
 public class ProductSpecification {
+    private ProductID upcCode;
+    private String description;
+    private BigDecimal price;
+
+    public ProductSpecification(ProductID upcCode, String description, BigDecimal price) {
+        this.upcCode = upcCode;
+        this.description = description;
+        this.price = price;
+    }
+
+    public ProductID getUpcCode() {
+        return upcCode;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setUpcCode(ProductID upcCode) {
+        this.upcCode = upcCode;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ProductSpecification ps = (ProductSpecification) o;
+        return this.upcCode == ps.upcCode && this.description == ps.description && this.price == ps.price;
+    }
 }
