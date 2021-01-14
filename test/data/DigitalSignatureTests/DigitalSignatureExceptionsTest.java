cd41aac9c85f8a34192b697ba9a9cf214ab5c2d8
Xavier <xaavier@protonmail.com>
2021-01-14 17:49:39 +0100

    ProductID and DSign excepcions tests done

--- test/data/DigitalSignatureTests/DigitalSignatureExceptionsTest.java
+++ test/data/DigitalSignatureTests/DigitalSignatureExceptionsTest.java
@@ -1,4 +1,30 @@
 package data.DigitalSignatureTests;
 
+import data.DigitalSignature;
+import exceptions.NullObjectException;
+import exceptions.WrongIdCodeFormat;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class DigitalSignatureExceptionsTest {
+
+    @Test
+    public void nullConstructorParameterTest() {
+        assertThrows(NullObjectException.class, () -> new DigitalSignature(null));
+    }
+
+    @Test
+    public void formatParameterTest() {
+        String name = "Xavier";
+        byte[] bArray = name.getBytes();
+        assertDoesNotThrow(() -> new DigitalSignature(bArray));
+    }
+
+    @Test
+    public void nullEqualsParameterTest() throws NullObjectException, WrongIdCodeFormat {
+        String name = "Xavier";
+        byte[] bArray = name.getBytes();
+        assertNotEquals(null, new DigitalSignature(bArray));
+    }
 }
--- test/data/ProductIdTests/ProductIdExceptionsTest.java
+++ test/data/ProductIdTests/ProductIdExceptionsTest.java
@@ -1,4 +1,28 @@
 package data.ProductIdTests;
 
+import data.ProductID;
+import exceptions.NullObjectException;
+import exceptions.WrongIdCodeFormat;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class ProductIdExceptionsTest {
+
+    @Test
+    public void nullConstructorParameterTest() {
+        assertThrows(NullObjectException.class, () -> new ProductID(null));
+    }
+
+    @Test
+    public void wrongFormatParameterTest() {
+        assertThrows(WrongIdCodeFormat.class, () -> new ProductID("0123456789AB"));
+        assertThrows(WrongIdCodeFormat.class, () -> new ProductID("AB2345678901"));
+        assertDoesNotThrow(() -> new ProductID("012345678901"));
+    }
+
+    @Test
+    public void nullEqualsParameterTest() throws NullObjectException, WrongIdCodeFormat {
+        assertNotEquals(null, new ProductID("012345678901"));
+    }
 }
